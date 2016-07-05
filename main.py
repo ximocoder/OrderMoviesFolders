e0fea6dd6ed124e22a5cd7e596783d7290d449fd
ximo <ximo.coder@gmail.com>
2016-07-05 21:00:59 +0200

    Some code cleaning

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # ximo.coder@gmail.com
 #
 import os
-import shutil as sh
 import tkinter as tk
 from tkinter.filedialog import askdirectory
 from tkinter import messagebox
@@ -18,10 +17,10 @@ class Application(tk.Frame):
         self.directory = ""
 
     def createWidgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Load Directory\n(click me)"
-        self.hi_there["command"] = self.askdir
-        self.hi_there.pack(side="top")
+        self.btnload = tk.Button(self)
+        self.btnload["text"] = "Load Directory\n(click me)"
+        self.btnload["command"] = self.askdir
+        self.btnload.pack(side="top")
 
         self.QUIT = tk.Button(self, text="QUIT", fg="red",
                               command=root.destroy)
@@ -32,10 +31,6 @@ class Application(tk.Frame):
 
         self.GO.pack(side="bottom")
 
-    def say_hi(self):
-        print("hola shurmanos!")
-        dir = askdirectory()
-
     def askdir(self):
         """Returns a selected directoryname."""
         res = askdirectory()
@@ -44,12 +39,16 @@ class Application(tk.Frame):
 
     def loadtree(self):
         listdir = os.walk(self.directory)
-        dirbase = "D:\\MoviesSD_Year\\"
+        dirbase = "D:\\Movies_Year\\"
         for x in listdir:
             mode = os.stat(x[0]).st_mode
-            if S_ISDIR(mode) and "VIDEOTECA" not in x[0] and ("dvdrip" or "xvid" in x[0].lower()):
-                # print(x)
-                y = x[0].split(self.directory, 1)[1]
+            if S_ISDIR(mode) and "VIDEOTECA" not in x[0]:  # and ("dvdrip" or "xvid" in x[0].lower()):
+                print(x[0])
+                dr = self.directory
+                if "\\" in x[0]:
+                    y = x[0].rsplit("\\", 1)[1]
+                else:
+                    y = ""
                 print("split: " + y)
 
                 if "198" in x[0]:
@@ -101,23 +100,6 @@ class Application(tk.Frame):
                     os.rename(x[0], dirbase + "2015\\" + y)
 
 
-                    # if "2010" in x[0] or "2011" in x[0] or "2012" in x[0] or "2013" or "2014" in x[0] \
-                    #         and "VIDEOTECA" not in x[0]:
-                    #     y = x[0].split(self.directory, 1)[1]
-                    #     print("split: " + y)
-                    #     try:
-                    #         os.rename(x[0], "D:\\Movies_Year\\2010\\" + y)
-                    #     except:
-                    #         tk.messagebox._show("ERROR", x[0])
-                    #
-                    # if "2015" in x[0] or "2016" in x[0]:
-                    #     y = x[0].split(self.directory, 1)[1]
-                    #     print("split: " + y)
-                    #     try:
-                    #         os.rename(x[0], "D:\\Movies_Year\\2015\\" + y)
-                    #     except:
-                    #         tk.messagebox._show("ERROR", x[0])
-
         tk.messagebox._show("Done", "Done")
 
 
@@ -125,18 +107,3 @@ root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
 
-# class TkFileDialogExample:
-#     def __init__(self):
-#         index = 0
-#
-#     def main(self):
-#         root = Tk()
-#         directory = self.askdirectory()
-#         Label(root, text=directory).pack()
-#         root.mainloop()
-#     if __name__ == "__main__":
-#         main()
-#
-#     def askdirectory(self):
-#         """Returns a selected directoryname."""
-#         return tkinter.filedialog(**self.dir_opt)
