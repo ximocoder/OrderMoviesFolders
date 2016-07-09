c0c701c6d25626611c47ec93027dcdb3f5420356
ximo <ximo.coder@gmail.com>
2016-07-09 23:17:36 +0200

    Code cleansing, check if a directory contains a movie

--- main.py
+++ main.py
@@ -35,72 +35,70 @@ class Application(tk.Frame):
         """Returns a selected directoryname."""
         res = askdirectory()
         self.directory = res
-        print(res)
+        #print(res)
 
     def loadtree(self):
         listdir = os.walk(self.directory)
         dirbase = "D:\\Movies_Year\\"
         for x in listdir:
             mode = os.stat(x[0]).st_mode
-            if S_ISDIR(mode) and "VIDEOTECA" not in x[0]:  # and ("dvdrip" or "xvid" in x[0].lower()):
-                print(x[0])
+            if S_ISDIR(mode) and "VIDEOTECA" not in x[0] and x[0] != self.directory:  # and ("dvdrip" or "xvid" in x[0].lower()):
+                #print(x[0])
                 dr = self.directory
                 if "\\" in x[0]:
                     y = x[0].rsplit("\\", 1)[1]
                 else:
                     y = ""
-                print("split: " + y)
+                #print("split: " + y)
 
-                if "198" in x[0]:
-                    # print(x[0], "D:\\Movies_Year\\" + y)
+                if self.CheckDirectoryIsMovie(x[0]):
+                    print(x[0])
 
-                    # sh.move(x[0], "D:\\Movies_Year\\1980\\" + y)
-                    os.rename(x[0], dirbase + "1980\\" + y)
+                    if "194" in x[0]:
+                        os.rename(x[0], dirbase + "_pre1950_\\" + y)
 
-                if "194" in x[0]:
-                    os.rename(x[0], dirbase + "pre1950\\" + y)
+                    if "195" in x[0]:
+                        os.rename(x[0], dirbase + "1950\\" + y)
 
-                if "195" in x[0]:
-                    os.rename(x[0], dirbase + "1950\\" + y)
+                    if "196" in x[0]:
+                        os.rename(x[0], dirbase + "1960\\" + y)
 
-                if "196" in x[0]:
-                    os.rename(x[0], dirbase + "1960\\" + y)
+                    if "197" in x[0]:
+                        os.rename(x[0], dirbase + "1970\\" + y)
 
-                if "197" in x[0]:
-                    os.rename(x[0], dirbase + "1970\\" + y)
+                    if "198" in x[0]:
+                        os.rename(x[0], dirbase + "1980\\" + y)
 
-                if "198" in x[0]:
-                    os.rename(x[0], dirbase + "1980\\" + y)
+                    if "199" in x[0]:
+                        os.rename(x[0], dirbase + "1990\\" + y)
 
-                if "199" in x[0]:
-                    os.rename(x[0], dirbase + "1990\\" + y)
+                    if "200" in x[0]:
+                        os.rename(x[0], dirbase + "2000\\" + y)
 
-                if "200" in x[0]:
-                    os.rename(x[0], dirbase + "2000\\" + y)
+                    if "2010" in x[0]:
+                        os.rename(x[0], dirbase + "2010\\" + y)
 
-                if "2010" in x[0]:
-                    os.rename(x[0], dirbase + "2010\\" + y)
+                    if "2010" in x[0] or "2011" in x[0] or "2012" in x[0] \
+                            or "2013" in x[0] or "2014" in x[0]:
+                        os.rename(x[0], dirbase + "2010\\" + y)
 
-                if "2011" in x[0]:
-                    os.rename(x[0], dirbase + "2010\\" + y)
+                    if "2015" in x[0]:
+                        os.rename(x[0], dirbase + "2015\\" + y)
 
-                if "2012" in x[0]:
-                    os.rename(x[0], dirbase + "2010\\" + y)
-
-                if "2013" in x[0]:
-                    os.rename(x[0], dirbase + "2010\\" + y)
-
-                if "2014" in x[0]:
-                    os.rename(x[0], dirbase + "2010\\" + y)
-
-                if "2015" in x[0]:
-                    os.rename(x[0], dirbase + "2015\\" + y)
-
-                if "2016" in x[0]:
-                    os.rename(x[0], dirbase + "2015\\" + y)
+                    if "2016" in x[0]:
+                        os.rename(x[0], dirbase + "2015\\" + y)
 
 
         tk.messagebox._show("Done", "Done")
+
+    def CheckDirectoryIsMovie(self, directory):
+        ismovie = False
+        extensions = [".avi", ".mkv", ".mp4"]
+
+        for root, dirnames, filenames in os.walk(directory):
+            for check in filenames:
+                if any(extension in check for extension in extensions):
+                    return True
 
 
 root = tk.Tk()
